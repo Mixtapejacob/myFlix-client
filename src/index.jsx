b562469917418361d89c939137c117d54ba1c48f
Mixtapejacob <144938167+Mixtapejacob@users.noreply.github.com>
2024-04-08 20:43:10 -0500

    Added files & code

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client';
 
+import { createRoot } from "react-dom/client";
+
+import { MainView } from "./components/main-view/main-view";
+
+import "./index.scss";
+
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
