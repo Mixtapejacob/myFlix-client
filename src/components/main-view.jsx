9165cf692b1b3370857369fb683036c99b967c42
Mixtapejacob <144938167+Mixtapejacob@users.noreply.github.com>
2024-04-11 15:48:17 -0500

    added components sign-up & login

--- src/components/main-view.jsx
+++ src/components/main-view.jsx
@@ -2,13 +2,16 @@ import { useState, useEffect } from "react";
 import { useState } from "react";
 import { MovieCard } from "./book-card";
 import { MovieView } from "./book-view";
+import { LoginView } from "../login-view/login-view";
+
 
 export const MainView = () => {
-  const [books, setBooks] = useState([]);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
-
-  const [selectedBook, setSelectedBook] = useState(null);
-
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4MTBlOTFkMzUyNzdiNWY0Y2NkNjMiLCJVc2VybmFtZSI6ImpvbmRvZTIiLCJQYXNzd29yZCI6IiQyYiQxMCRlSFB0RWZ3b1VNQnNOTHNROG5meHJ1MUZNc2piR08xbEkxVnJXZzFnTUl3NDQyR0RaWlBnaSIsIkVtYWlsIjoiam9uZG9lMkBtYWlsLmNvbSIsIkJpcnRoZGF5IjoiMjAwMS0wMS0wMVQwMDowMDowMC4wMDBaIiwiRmF2b3JpdGVNb3ZpZXMiOltdLCJfX3YiOjAsImlhdCI6MTcxMjg1MzIzMiwiZXhwIjoxNzEzNDU4MDMyLCJzdWIiOiJqb25kb2UyIn0.AfZ_2e7I0yH8mEEVHzg-c7V0PIgkz07f48flYEtUbr4";
